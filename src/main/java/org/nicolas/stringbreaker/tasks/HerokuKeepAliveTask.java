2611bfb2d1ae40ebddf9a16dd01705ad8673ca02
nicolas.silva <nicolas-md@hotmail.com>
2018-03-01 13:33:41 -0300

    Alterando frequência do request de keepAlive para o Heroku (seg-sex, cada 15min das 8h às 18h)

--- src/main/java/org/nicolas/stringbreaker/tasks/HerokuKeepAliveTask.java
+++ src/main/java/org/nicolas/stringbreaker/tasks/HerokuKeepAliveTask.java
@@ -5,14 +5,17 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class HerokuKeepAliveTask {
-    @Scheduled(fixedRate = 300000)
+    @Scheduled(cron = "00,15,30,45 8-18 * * 1-5")
     public void keepAlive() {
         try {
             Unirest.get("https://stringbreaker.herokuapp.com").asString();
             Unirest.get("https://stringbreaker-api.herokuapp.com").asString();
-            System.out.println("Sending keepAlive request to Heroku: DONE");
+            System.out.println("Sending keepAlive request to Heroku at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy")));
         } catch (UnirestException e) {
             e.printStackTrace();
         }
